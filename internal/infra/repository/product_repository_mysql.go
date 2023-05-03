f9ead4773fdd8e1b3bf5c462e290fad9b7e1836e
Gustavo Eyros <gustavoeyros@hotmail.com>
2023-05-03 17:20:07 -0300

    feat: FindAll function

--- internal/infra/repository/product_repository_mysql.go
+++ internal/infra/repository/product_repository_mysql.go
@@ -22,3 +22,26 @@ func (p *ProductRepositoryMySql) Create(product *entities.Product) error {
 	}
 	return nil
 }
+
+func (p *ProductRepositoryMySql) FindAll() ([]*entities.Product, error) {
+	rows, err := p.DB.Query("SELECT id, name, price FROM Products")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var products []*entities.Product
+
+	for rows.Next() {
+		var product entities.Product
+		err = rows.Scan(&product.ID, &product.Name, &product.Price)
+		if err != nil {
+			return nil, err
+		}
+		products = append(products, &product)
+
+	}
+	return products, nil
+}
